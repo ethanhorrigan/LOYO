7bb72f4a35f7b8f2837a9dfae5d5ebb59d062de5
EthanHorrigan <ethanhorr@gmail.com>
2020-02-02 16:48:41 +0000

    Registration Backend Change

--- server/models/user.js
+++ server/models/user.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema
 
 const userSchema = new Schema ({
     email: String,
+    summonerName: String,
     password: String
 })
 
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,11 +1,35 @@
 const express = require('express')
 const router = express.Router()
-
+const User = require('../models/user')
 const mongoose = require('mongoose')
-const db =
+const db = "mongodb://ethan:ethan123@ds215093.mlab.com:15093/fyp-users"
+
+
+mongoose.connect(db, err => {
+    if(err) {
+        console.log('Error: ' + err)
+    }
+    else {
+        console.log('Connected to the DB')
+    }
+})
 
 router.get('/', (req, res) => {
     res.send('From API Route')
 })
 
+// Register Post Request
+
+router.post('/register', (req, res) => {
+    let userData = req.body
+    let user = new User(userData)
+    user.save((error, registeredUser) => {
+        if (error) {
+            console.log(error)
+        }
+        else {
+            res.status(200).send(registeredUser)
+        }
+    })
+})
 module.exports = router
