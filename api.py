1ef4a66953c1a7577e6bd5c6e4899f5bbb4c3196
EthanHorrigan <ethanhorr@gmail.com>
2020-03-09 19:23:52 +0000

    Documentation: API

--- api.py
+++ api.py
@@ -54,6 +54,13 @@ class PasswordSetup:
 
 class Players(Resource):
     def get(self):
+        """
+        Retrieves the players in-game statistics.
+
+        Returns:
+        Players Stats in JSON format.
+
+        """
         conn = db_connect.connect()  # connect to the db
         query = conn.execute(
             "select summonerName, rank, tier, wins, losses, primaryRole, secondaryRole from players")
@@ -64,6 +71,13 @@ class Players(Resource):
 
 class PlayerStandings(Resource):
     def get(self):
+        """
+        Get the leaderboards from the database.
+
+        Returns:
+        Leaderboards from the database.
+
+        """
         conn = db_connect.connect()  # connect to the db
         query = conn.execute("SELECT * FROM Players ORDER BY wins DESC;")
         result = {'players': [dict(zip(tuple(query.keys()), i))
@@ -73,6 +87,14 @@ class PlayerStandings(Resource):
 
 class Lobby(Resource):
     def post(self):
+        """
+        Inserts a user to the lobby, the lobby is used to hold players,
+        before matchmaking takes place.
+
+        Returns:
+        Inserts a user to the lobby and returns the data in JSON.
+
+        """
         conn = db_connect.connect()  # connect to the db
         SummonerName = request.json['summonerName'] # Get SummonerName
         _rank = Summoner.get_rank_string(self, SummonerName) # Get rank
@@ -82,21 +104,36 @@ class Lobby(Resource):
         conn.execute("insert into Lobby values(null,'{0}', '{1}', '{2}')".format(SummonerName, _rank, _mmr))
         return request.json
     def get(self):
+        """
+        Get the amount of players currently in the lobby, to verify
+        if enough players are avaialable for a match to begin.
+
+        Returns:
+        hashed version of the users password.
+
+        """       
         conn = db_connect.connect()
-        query = conn.execute(
-            "select COUNT(summonerName) from Lobby")
+        query = conn.execute("select COUNT(summonerName) from Lobby")
         qResult = query.cursor.fetchall()
         playerCount = qResult[0][0]
         if playerCount <= 10:
             playerCount = qResult[0][0]
         else:
             playerCount = "FULL"
-        # return request.json
         return playerCount
 
 
 class Users(Resource):
     def get(self):
+        """
+        Validates a password with the corresponding hashed password.
+
+        Args:
+        player: The password .
+        Returns:
+        hashed version of the users password.
+
+        """       
         conn = db_connect.connect()  # connect to database
         # This line performs query and returns json result
         query = conn.execute("select username from users")
@@ -104,6 +141,15 @@ class Users(Resource):
         return {'users': [i[0] for i in query.cursor.fetchall()]}
 
     def post(self):
+        """
+        Validates a password with the corresponding hashed password.
+
+        Args:
+        player: The password .
+        Returns:
+        hashed version of the users password.
+
+        """       
         conn = db_connect.connect()  # connect to the db
         Username = request.json['username']
         SummonerName = request.json['summonerName']
@@ -180,21 +226,15 @@ class MatchMaking(Resource):
         
         query = conn.execute("select summonerName FROM Lobby order by mmr desc") # Get the Users from the Lobby
 
-        
-        
-        uuid = UUIDGenerator.generate_uuid()
+        uuid = UUIDGenerator.generate_uuid() # Generate a Unique ID for the match.
 
-        # Add Players in Lobby to the matchmaking queue
-        results = query.cursor.fetchall()
+        results = query.cursor.fetchall() # Get the players currently in the lobby.
         count = 0
         while matching_state:
             # for i in results:
             _summoner_name_1 = results[count][0]
             count+=1
             _summoner_name_2 = results[count][0]
-            print("Summoner Name: {0}".format(_summoner_name_1))
-            print("Summoner Name: {0}".format(_summoner_name_2))
-            print("UUID: {0}".format(uuid))
             conn.execute("insert into Match values('{0}', '{1}', '{2}')".format(1, _summoner_name_1, uuid))
             conn.execute("insert into Match values('{0}', '{1}', '{2}')".format(2, _summoner_name_2, uuid))
             # _rank_str = Summoner.get_rank_string(self, _summonerName)
@@ -212,48 +252,22 @@ class MatchMaking(Resource):
 
         return {'match': [dict(zip(tuple(match_query.keys()), i)) for i in match_query.cursor]}
 
-class Employees(Resource):
-    def get(self):
-        conn = db_connect.connect()  # connect to database
-        # This line performs query and returns json result
-        query = conn.execute("select * from employees")
-        # Fetches first column that is Employee ID
-        return {'employees': [i[0] for i in query.cursor.fetchall()]}
 
-class Employees_Name(Resource):
-    def get(self, employee_id):
-        conn = db_connect.connect()
-        query = conn.execute(
-            "select * from employees where EmployeeId =%d " % int(employee_id))
-        result = {'data': [dict(zip(tuple(query.keys()), i))
-                           for i in query.cursor]}
-        response = jsonify(result)
-        return response
 
 
 api.add_resource(Players, '/players')  # Route_1
 api.add_resource(PlayerStandings, '/playerstandings')  # Route_2
 api.add_resource(Lobby, '/lobby')  # Route_3
-api.add_resource(Employees_Name, '/employees/<employee_id>')  # Route_3
 api.add_resource(Users, '/users')  # Route_4
-api.add_resource(UsersName, '/users/<username>')  # Route_3
-api.add_resource(Login, '/login')  # Login Route
-api.add_resource(MatchMaking, '/mm')  # Matchmaking Route
+api.add_resource(UsersName, '/users/<username>')  # Route_5
+api.add_resource(Login, '/login')  # Route_6
+api.add_resource(MatchMaking, '/mm')  # Route_7
 
 # Methods
 
 
 def getSummoner(player):
-        # Connect to the database
-        # conn = db_connect.connect()
-        # Search for the Summoner
     summonerDetails = Summoner.get_player_details(player)
-    # print(summonerDetails)
-    # Check if the Summoner Exists
-    # Return Summoner Data
-    # Retrieve SummonerID
-    # Insert SummonerID Into USERS Table for the given summoner
-
 
 
 if __name__ == '__main__':
