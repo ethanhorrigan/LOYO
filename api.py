90f8be59dd10b2c5fe22d57363aefe105baa965a
EthanHorrigan <ethanhorr@gmail.com>
2020-04-10 19:07:31 +0100

    Create

--- api.py
+++ api.py
@@ -574,7 +574,7 @@ api.add_resource(Users, '/users')  # Route_5
 api.add_resource(SummonerName, '/s/<username>')  # Route_5
 api.add_resource(Login, '/login')  # Route_6
 api.add_resource(MatchMaking, '/mm')  # Route_7
-api.add_resource(Createm, '/login')  # Route_8
+api.add_resource(CreateMatch, '/create')  # Route_8
 
 if __name__ == '__main__':
     app.run(port='5002')
