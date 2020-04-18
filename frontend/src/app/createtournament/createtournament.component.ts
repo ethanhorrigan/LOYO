4ab68c27374e76b5f731d330b15de47e37da586f
EthanHorrigan <ethanhorr@gmail.com>
2020-04-18 22:31:44 +0100

    disable all dates on the calendar before min & mix

--- frontend/src/app/createtournament/createtournament.component.ts
+++ frontend/src/app/createtournament/createtournament.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-createtournament',
@@ -7,18 +9,61 @@ import { FormGroup, Validators, FormBuilder } from '@angular/forms';
   styleUrls: ['./createtournament.component.scss']
 })
 export class CreatetournamentComponent implements OnInit {
-  registerForm: FormGroup;
+  createForm: FormGroup;
   //formBuilder: any;
 
-  constructor(private formBuilder: FormBuilder) { }
+  //Blocking Variables
+
+  minDate: Date;
+  maxDate: Date;
+
+  day: number;
+  month: string;
+  year: number;
+
+  date: Date;
+
+  finalDate: any;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    ) { 
+
+      //In constructor
+      const currentYear = new Date().getFullYear();
+      const currentDay = new Date().getDate();
+      const currentMonth = new Date().getMonth();
+      this.minDate = new Date(currentYear, currentMonth, currentDay)
+      this.maxDate = new Date(currentYear, 11, 31);
+      //end of constructor
+    }
+
 
   ngOnInit() {
-    this.registerForm = this.formBuilder.group({
-      username: ['', Validators.required],
-      summonerName: ['', Validators.required],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      role: ['', Validators.required]
+    this.createForm = this.formBuilder.group({
+      // Admin is Current user logged in
+      gameName: [,  Validators.required],
+      date: [{value: 'Select Date', disabled: true}, Validators.required],
+      gameMode: ['', Validators.required]
   });
   }
+
+
+  get form() { return this.createForm.controls; }
+
+  
+
+  onSubmit() {
+    this.date = this.form.date.value;
+    this.finalDate = this.date.getFullYear() + "-" + this.date.getDate() + "-" + this.date.getMonth()
+    console.log(this.date.getUTCDay());
+    
+  }
+
+  
+
+
 
 }
