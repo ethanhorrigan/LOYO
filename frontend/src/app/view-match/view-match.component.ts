43b4f315920c38aa22604a0c657421f90643781c
EthanHorrigan <ethanhorr@gmail.com>
2020-04-19 16:14:37 +0100

    viewmatch compo

--- frontend/src/app/view-match/view-match.component.ts
+++ frontend/src/app/view-match/view-match.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { TestGameComponent } from '../test-game/test-game.component';
 
 @Component({
   selector: 'app-view-match',
@@ -11,9 +12,13 @@ export class ViewMatchComponent implements OnInit, OnDestroy {
   private sub: any;
   matchId: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private gameComponent: TestGameComponent) { }
 
   ngOnInit() {
+
+    this.gameComponent.game
     this.sub = this.activatedRoute.params.subscribe(params => {
       this.matchId = params['matchId'];
     });
